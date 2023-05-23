5cb97435420bf6c5e81072e31d7a9cdb7a20ca00
seoweed <darkweed1106@gmail.com>
2023-05-23 23:38:24 +0900

    2023-05-23 23:38 commit: 홈화면 추가, 회원 등록, 회원 조회 기능 추가

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+// DI 방법 2. 스프링 빈 등록
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
